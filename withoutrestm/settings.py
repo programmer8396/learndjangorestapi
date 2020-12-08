9ae0767a4721d8cc8aec2e41f843240e682bfeb3
programmer8396 <57538011+programmer8396@users.noreply.github.com>
2020-12-08 23:34:28 +0530

    Edited debug

--- withoutrestm/settings.py
+++ withoutrestm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c9w76qx%$3#)i!l&)4ioqu+o1axc-gkaesyue9%#-4+m^q$sos'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = False    
 
 ALLOWED_HOSTS = []
 
